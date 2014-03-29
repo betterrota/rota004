b6e0b421409f30f8c233df53018e51008388e899
OpenShift guest <riahim@lsbu.ac.uk>
2014-03-29 13:53:05 -0400

    module user

--- module/Users/config/module.config.php
+++ module/Users/config/module.config.php
@@ -2,7 +2,26 @@
 return array(
     'controllers' => array(
         'invokables' => array(
-            'Users\Controller\Users' => 'Users\Controller\UserController',
+            'Users\Controller\User' => 'Users\Controller\UserController',
+        ),
+    ),
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'user' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/user[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Users\Controller\User',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
         ),
     ),
     'view_manager' => array(
